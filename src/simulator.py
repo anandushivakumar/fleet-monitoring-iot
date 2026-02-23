6f6b6fe0546a5b666ff4eb813270e9566a270a06
anandushivakumar <anandu.shivakumar@gmail.com>
2026-02-23 17:17:45 +0400

    Implemented MQTT publishing with .env file for security

--- src/simulator.py
+++ src/simulator.py
@@ -1,26 +1,35 @@
 import random
 import time
 import json
+import ssl
+import os
 from datetime import datetime, timezone
+import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+
+# load env variables
+# hivemqtt broker URL, port, username and password for web-client
+load_dotenv()
+
+MQTT_BROKER = os.getenv("MQTT_BROKER")
+MQTT_PORT = int(os.getenv("MQTT_PORT", 8883))
+MQTT_USERNAME = os.getenv("MQTT_USERNAME")
+MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
 
 # config
-NUM_VEHICLES = 500 # number of vehicles tracked
-PUBLISH_INTERVAL = 5  # publish data every 5s
+NUM_VEHICLES = 3
+PUBLISH_INTERVAL = 5  # seconds
 
-# Base GPS location (ex: depot)
 BASE_LAT = 25.276987
 BASE_LON = 55.296249
 
-# Speed limits (km/h)
-MIN_SPEED = 40 
-MAX_SPEED = 140 
+MIN_SPEED = 40
+MAX_SPEED = 140
 
-
+# generate random data
 def generate_vehicle_data(vehicle_id: str) -> dict:
-    # generate simulated telemetry data for a single vehicle.
     speed = round(random.uniform(MIN_SPEED, MAX_SPEED), 2)
 
-    # gmall random movement around base location
     latitude = round(BASE_LAT + random.uniform(-0.02, 0.02), 6)
     longitude = round(BASE_LON + random.uniform(-0.02, 0.02), 6)
 
@@ -34,10 +43,27 @@ def generate_vehicle_data(vehicle_id: str) -> dict:
         "timestamp": timestamp
     }
 
+# attempt connection to MQTT broker
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to HiveMQ Cloud successfully!")
+    else:
+        print(f"Connection failed with code {rc}")
+
 
 def main():
-    print("Simulator Start")
-    print(f"Simulating {NUM_VEHICLES} vehicles")
+    if not all([MQTT_BROKER, MQTT_USERNAME, MQTT_PASSWORD]):
+        print("ERROR: Missing MQTT credentials in .env file")
+        return
+
+    client = mqtt.Client()
+    client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+
+    client.tls_set(cert_reqs=ssl.CERT_REQUIRED)
+    client.on_connect = on_connect
+
+    client.connect(MQTT_BROKER, MQTT_PORT)
+    client.loop_start()
 
     vehicle_ids = [f"VEHICLE_{i+1:03d}" for i in range(NUM_VEHICLES)]
 
@@ -45,11 +71,19 @@ def main():
         while True:
             for vehicle_id in vehicle_ids:
                 data = generate_vehicle_data(vehicle_id)
-                print(json.dumps(data))
+
+                topic = f"fleet/vehicle/{vehicle_id}/telemetry"
+                payload = json.dumps(data)
+
+                client.publish(topic, payload)
+                print(f"Published → {topic}")
+
             time.sleep(PUBLISH_INTERVAL)
 
     except KeyboardInterrupt:
-        print("\nSimulator stopped.")
+        print("\nStopping simulator...")
+        client.loop_stop()
+        client.disconnect()
 
 
 if __name__ == "__main__":
